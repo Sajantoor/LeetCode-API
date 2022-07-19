b2b077910139c787bf0aabf0202764007131d3cc
Sajan Toor <sajantoor98@gmail.com>
2022-07-18 21:11:06 -0700

    Init app

---
+++ src/main/java/com/leetcode/leetcodeapi/LeetcodeApiApplication.java
@@ -0,0 +1,11 @@
+package com.leetcode.leetcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/leetcode/leetcodeapi/LeetcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.leetcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
